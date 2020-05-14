4b01de8618be20c34492567e75a1470495bcacaa
Isaac T <ijeffers@in.ibm.com>
2020-05-14 11:08:50 +0530

    Initial Commit

---
+++ src/main/java/com/gm/chess/GmChessAppApplication.java
@@ -0,0 +1,13 @@
+package com.gm.chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmChessAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmChessAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gm/chess/GmChessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.chess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmChessAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
